16cfc2f9732ee96af740752c35efaa49191ef0cb
h3cth0r <hectorsucre13@gmail.com>
2024-06-27 12:11:20 -0600

    nice zebra design

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,32 +1,17 @@
 <script>
-  // You can add any JavaScript you need here
+  // Define the number of lines
+  let lines = Array(40).fill(0);
 </script>
 
 <div class="main-container">
+  <div class="footer-content"></div>
   <div class="zebra-container">
-    <div class="footer-content">
-      <div class="content-left">
-        <!-- social network list -->
-        <!-- zentiel c 2024 -->
-      </div>
-      <div class="content-right">
-        <div class="newsletter">
-          <div class="newsletter-content">
-            <p>Newsletter. Sign up to stay up to date</p>
-            <div class="newsletter-inputs">
-              <input type="email" name="email">
-              <button>Send</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="zebra-lines">
-      <!-- Generate the zebra lines using CSS -->
-      {#each Array(30) as _, i}
-        <div class="zebra-line" style="height: {2 + i}px; width: {100 - i * 2}%;"></div>
-      {/each}
-    </div>
+    {#each lines as _, index}
+      <div
+        class="zebra-line"
+        style="--margin: {(40 - index) * 0.04}%; --height: {index * 0.04}%;"
+      ></div>
+    {/each}
   </div>
 </div>
 
@@ -34,39 +19,28 @@
   .main-container {
     background-color: white;
     width: 100vw;
-    height: 100vh;
+    height: 150vh;
     display: flex;
     flex-direction: column;
-    justify-content: flex-end;
-  }
-  .zebra-container {
-    background-color: black;
-    position: relative;
   }
   .footer-content {
-    display: flex;
-    justify-content: space-between;
-    padding: 20px;
-    color: white;
+    width: 100%;
+    height: 30%;
+    background-color: black;
   }
-  .newsletter {
+  .zebra-container {
     display: flex;
     flex-direction: column;
-  }
-  .newsletter-inputs {
-    display: flex;
-  }
-  .newsletter-inputs input {
-    margin-right: 10px;
-  }
-  .zebra-lines {
-    position: relative;
-    overflow: hidden;
+    align-items: center;
     width: 100%;
+    height: 70%;
+    background-color: white;
   }
   .zebra-line {
-    background-color: white;
-    margin-bottom: 2px;
+    background-color: black;
+    width: 100%;
+    height: var(--margin);
+    margin-top: var(--height);
   }
 </style>
 
