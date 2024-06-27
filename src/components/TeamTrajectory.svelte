8d7304977d2d1e355339790284c85ee89fbaee5b
h3cth0r <hectorsucre13@gmail.com>
2024-06-26 22:45:45 -0600

    added the trajectory section

---
+++ src/components/TeamTrajectory.svelte
@@ -0,0 +1,104 @@
+<script>
+  import { onMount } from 'svelte';
+  import TrajectoryCard from "./TrajectoryCard.svelte";
+
+  let activateAnimation = false;
+  let colsContainer;
+
+  onMount(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          activateAnimation = true;
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.5 });
+
+    observer.observe(colsContainer);
+
+    return () => {
+      observer.disconnect();
+    };
+  });
+
+</script>
+
+<div
+  class="main-container"
+>
+  <div
+    class="content-container"
+  >
+    <div
+      class="header-container"
+    >
+      <p>Unlocking Success Through Our Portfolio</p>
+    </div>
+    <div
+      class="columns-container"
+      bind:this={colsContainer}
+    >
+      <div class="column-container">
+        <TrajectoryCard 
+          cardNumber={30}
+          labeOperator={" +"}
+          cardLabel={"projects"}
+          description={"Long record of software solutions."}
+          run_animation={activateAnimation}
+        />
+      </div>
+      <div class="column-container">
+        <TrajectoryCard 
+          cardNumber={20}
+          labeOperator={"K+"}
+          cardLabel={"hours"}
+          description={"Our developers have dedicated long hours to deliver great products."}
+          run_animation={activateAnimation}
+        />
+      </div>
+      <div class="column-container">
+        <TrajectoryCard 
+          cardNumber={30}
+          labeOperator={" +"}
+          cardLabel={"partners"}
+          description={"We have collaborated with more than 30 outstanding partners."}
+          run_animation={activateAnimation}
+        />
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .main-container{ 
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      height: 50vh;
+      padding: 2rem;
+      background-color: black;
+      font-family: Arial, sans-serif;
+  }
+  .content-container {
+      width: 50%;
+      height: 85%;
+  }
+  .header-container {
+    width: 100%;
+    font-family: "Times New Roman", Times, serif;
+    font-size: 40px;
+  }
+  .columns-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    width: 100%;
+  }
+  .column-container {
+    width: 13%; 
+  }
+</style>
---
+++ src/components/TrajectoryCard.svelte
@@ -0,0 +1,66 @@
+<script>
+  export let cardNumber = 45;
+  export let labeOperator = "K+";
+  export let cardLabel = "projects";
+  export let description = "ine height is a CSS property that is used to control the vertical space between lines of text. It specifies the distance between the baselines of consecutive lines of text. This can make the text more readable and aesthetically pleasing by adjusting the spacing between lines. Here is an example of how line height can be set in CSS.";
+  export let run_animation = false;
+
+
+  let currentNumber = 0;
+  const startAnimation = () => {
+    let increment = Math.ceil(cardNumber / 5); // Adjust the speed of incrementing
+    let interval = setInterval(() => {
+      currentNumber += increment;
+      if (currentNumber >= cardNumber) {
+        currentNumber = cardNumber;
+        clearInterval(interval);
+      }
+    }, 200); // Adjust the speed of the animation
+  };
+
+  $: if(run_animation){
+    startAnimation();
+    run_animation = false;
+  }
+
+</script>
+
+<div
+  class="main-container"
+>
+  <div
+    class="label-container"
+  >
+    <div
+      class="units-container"
+    >
+      <p>{ currentNumber }{ labeOperator }</p>
+      <p>{ cardLabel }</p>
+    </div>
+  </div>
+  <div
+    class="description-container"
+  >
+    <p>{ description }</p>
+  </div>
+</div>
+
+<style>
+  .main-container {
+    width: 100%; 
+  }
+  .units-container {
+    font-family: "Times New Roman", Times, serif;
+    font-size: 35px;
+  }
+  .units-container p{
+    margin: 0;
+    padding: 0;
+  }
+  .description-container {
+  }
+  .description-container p {
+    line-height: 2.5;
+    color: #CACACA;
+  }
+</style>
